6d0b4c30e0ea302d4719f497cae6a324c788327c
Sharmina Akhtar <sakht318@gmail.com>
2018-10-16 23:53:14 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,10 +3,26 @@ import React from 'react';
 
 export default class DigitalClicker extends React.Component{
 
+  constructor() {
+    super();
+ 
+    // Define the initial state:
+    this.state = {
+      timesClicked: 0,
+    };
+  }
+
+  handleClick = () => {
+    timesClicked: +1
+}
+
   render(){
 
     return(
-
+      <div>
+       <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+       <button onClick={this.handleClick}>Click me!</button>
+     </div>
 
     )
   }
