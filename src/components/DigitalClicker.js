820ae5bf6f92a86418979a34124b713f21396b35
Sharmina Akhtar <sakht318@gmail.com>
2018-10-17 00:17:07 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -22,8 +22,7 @@ export default class DigitalClicker extends React.Component{
 
     return(
       <div>
-       <p>I have {this.state.timesClicked ? null : 'not'} been clicked!</p>
-       <button onClick={this.handleClick}>Click me!</button>
+       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
      </div>
 
     )
